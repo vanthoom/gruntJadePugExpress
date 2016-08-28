9bdfd5e791b5e060af661b49487df3c3e44d2df2
vanthoom <mr.vanthoom@gmail.com>
2016-08-28 12:20:41 +0200

    pug view engine

--- src/express.js
+++ src/express.js
@@ -4,13 +4,21 @@ var path = require('path');
 var http = require('http');
 
 app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+
+app.engine('jade', require('jade').__express);
+app.engine('pug', require('pug').__express);
+
 app.use(express.static(path.join(__dirname)));
 app.use('/lib', express.static(__dirname + '/../lib'));
 app.use('/src', express.static(__dirname + '/../src'));
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get(/\/(.*)/, function (req, res) {
+  res.render(req.params[0]);
 });
 
 var server = http.createServer(app).listen(3000, function(){
